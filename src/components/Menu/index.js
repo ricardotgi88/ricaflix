40d7aaa458ab86105009c0ee6f5a453edc6ea8dd
Ricardo Henrique dos Santos <ricardotgi88@hotmail.com>
2020-07-28 17:59:41 -0300

    cadastro de video cadastro de categoria

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import Logo from '../../assets/image/Logo.png';
 import './Menu.css';
 import Button from '../Button';
@@ -6,10 +7,10 @@ import Button from '../Button';
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <a to="/">
                 <img className="Logo" src={Logo} alt="Ricaflix Logo" />
             </a>
-            <Button as="a" className="ButtonLink" href="/">
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo Video
             </Button>
         </nav>
